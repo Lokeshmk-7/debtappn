9a88d48e3850f8903b70a7d24c5c1ac4609aeaf4
geek-loki <43682585+geek-loki@users.noreply.github.com>
2023-12-20 12:38:17 +0100

    Modified App and TestApp class
    Modified App and TestApp class to include the application execution functionality as well as application test functionality

--- src/main/java/com/debtapp/App.java
+++ src/main/java/com/debtapp/App.java
@@ -1,10 +1,20 @@
 package com.debtapp;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public final class App {
     private App() {
+    }
+
+    public static double calclateMonthlyInterest(double debtValue, double interestRate){
+        return (debtValue * interestRate)/(12*100);
+    }
+
+    public static double calculateOverallDebt(double debtValue, double monthlyRate, int paybackDuration){
+        return debtValue + monthlyRate * paybackDuration;
     }
 
     /**
@@ -13,5 +23,22 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter debt value (in Euros): ");
+        double debtValue = scanner.nextDouble();
+
+        System.out.println("Enter interest rate (in Percentage): ");
+        double interestRate = scanner.nextDouble();
+
+        System.out.println("Enter payback duration (in Months): ");
+        int paybackDuration = scanner.nextInt();
+
+        double monthlyInterest = calclateMonthlyInterest(debtValue, interestRate);
+        double overallDebt = calculateOverallDebt(debtValue, monthlyInterest, paybackDuration);
+
+        System.out.println("\nMonthly interest: " + monthlyInterest);
+        System.out.println("\nOverall debt: " + overallDebt + " Euros");
+
     }
 }
--- src/test/java/com/debtapp/AppTest.java
+++ src/test/java/com/debtapp/AppTest.java
@@ -15,4 +15,16 @@ class AppTest {
     void testApp() {
         assertEquals(1, 1);
     }
+
+    @Test
+    public void testcalclateMonthlyInterest(){
+        double monthlyInterest = App.calclateMonthlyInterest(1000, 12);
+        assertEquals(10.0, monthlyInterest);
+    }
+
+    @Test
+    public void testcalculateOverallDebt(){
+        double overallDebt = App.calculateOverallDebt(1000, 10.0, 12);
+        assertEquals(1120.0, overallDebt);
+    }
 }
